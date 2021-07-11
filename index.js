565355f62a2126eb9253e0e8917f5f70a392601c
pamtel <pamelaakosahiante@gmail.com>
2021-07-11 23:08:30 +0100

    added model and route folder

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = express();
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
+app.use(express.json());
 
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose
+  .connect(process.env.DATABASE_URL, {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -13,6 +17,9 @@ mongoose.connect(process.env.DATABASE_URL, {
   .then(console.log("DATABASE CONNECTED"))
   .catch(err => console.log(err))
 
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
 app.listen("5000", () => {
-    console.log("Backend is running...");
+    console.log("Backend is running on port 5000...");
 })
